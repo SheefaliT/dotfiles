e0598774575bca968bf74c8e22f9a705cbd12a31
Jason Roth <jason.roth@livingsocial.com>
2013-03-05 15:09:18 -0700

    add ssh wrapper that switches themes

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -23,3 +23,10 @@ colordump() {
     printf '\n\n     e.g., "\\e[41mTEXT\\e[0m" '
     printf "\e[41m(for TEXT like this)\e[0m\n"
 }
+
+ssh() {
+    local previous_profile=$ITERM_PROFILE
+    echo -e "\033]50;SetProfile=SSH\a"
+    /usr/bin/ssh "$@"
+    echo -e "\033]50;SetProfile=$previous_profile\a"
+}
