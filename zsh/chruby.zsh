341e7b3d0882148305fb52c14d5ec619cb007ac2
Jason Roth <jasonmichaelroth@gmail.com>
2014-01-04 11:41:41 -0700

    condition the ruby version managers

--- zsh/chruby.zsh
+++ zsh/chruby.zsh
@@ -1,2 +1,4 @@
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+if hash chruby 2> /dev/null; then
+    source /usr/local/opt/chruby/share/chruby/chruby.sh
+    source /usr/local/opt/chruby/share/chruby/auto.sh
+fi
